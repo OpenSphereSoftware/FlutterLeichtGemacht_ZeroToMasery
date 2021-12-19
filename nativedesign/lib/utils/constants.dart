4ea4698a7a5af98867a6d017225877090ea6a8ea
Max Berktold <max.berktold@opensphere.software>
2021-12-19 12:32:10 +0100

    added project for native design

---
+++ nativedesign/lib/utils/constants.dart
@@ -0,0 +1,15 @@
+const padding_20 = 20.0;
+const padding_10 = 10.0;
+const padding_50 = 50.0;
+const minimumPadding = 5.0;
+const String txtDashboard = "Dashboard";
+const String txtFeeds = "List";
+const String txtSettings = "Settings";
+const String usernameError = "Please enter valid username";
+const String username = "User name";
+const String passwordError = "Please enter valid password";
+const String password = "Password";
+const String unPwError = "Please enter both user name and password";
+const String login = "Login";
+const String alert = "Alert";
+const String ok = "Ok";
