80a0bc2dd75b37bd6e7af652adbd3c718349f92b
Max Berktold <max.berktold@opensphere.software>
2022-06-04 13:02:23 +0200

    support ios simulator, updates for better userflow

--- todo_redesign/lib/main.dart
+++ todo_redesign/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/firebase_options.dart';
 import 'package:todo/injection.dart' as di;
 import 'package:todo/presentation/routes/router.gr.dart' as r;
 import 'package:todo/theme.dart';
@@ -10,7 +11,7 @@ import 'injection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await di.init();
   runApp(MyApp());
 }
