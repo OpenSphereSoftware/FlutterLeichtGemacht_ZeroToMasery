9bb78db8d976577b521afa63b9ec2e39ce7bc333
Max Berktold <max.berktold@opensphere.software>
2022-08-27 10:34:29 +0200

    flutter grundlagen upgrade

--- flutter_grundlagen/android/app/src/main/kotlin/com/example/fgrund/MainActivity.kt
+++ flutter_grundlagen/android/app/src/main/kotlin/com/example/fgrund/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_grundlagen
+package com.example.fgrund
 
 import io.flutter.embedding.android.FlutterActivity
 
--- flutter_grundlagen/android/app/src/main/kotlin/com/example/fluttergrundlagen/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fluttergrundlagen
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
